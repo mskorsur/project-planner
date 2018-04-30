cd7a8a1595f1e23dc1920079d1234e6133b564aa
mskorsur <m.skorsur@gmail.com>
2018-04-30 17:39:38 +0200

    Update user reducer to handle add/remove project actions

--- client-app/src/redux/actions/userActions.js
+++ client-app/src/redux/actions/userActions.js
@@ -1,6 +1,7 @@
 import { ADD_USER, UPDATE_USER } from './actionTypes';
 import { generateId } from '../../utils/UuidGenerator';
 
+//payload should contain userName, password, firstName, lastName, email, organization and github link
 export function addUser(data) {
     return {
         type: ADD_USER,
--- client-app/src/redux/reducers/userReducers.js
+++ client-app/src/redux/reducers/userReducers.js
@@ -1,4 +1,7 @@
-import { ADD_USER, UPDATE_USER, ADD_PROJECT, REMOVE_PROJECT } from '../actions/actionTypes';
+import { ADD_USER, 
+         UPDATE_USER, 
+         ADD_PROJECT, 
+         REMOVE_PROJECT } from '../actions/actionTypes';
 
 const initialState = {
     byId: {},
@@ -7,7 +10,6 @@ const initialState = {
 
 //CASE REDUCERS
 function addUserById(byIdState, action) {
-    //payload should contain userName, password, firstName, lastName, email, organization and github link
     const { payload } = action;
     const newUser = {
         id: action.id,
@@ -29,6 +31,31 @@ function updateUserById(byIdState, action) {
     });
 }
 
+function addProjectToUser(byIdState, action) {
+    const userId = action.payload.user;
+    const projectId = action.id;
+
+    const userProjectsWithNewProject = [...byIdState[userId].projects, projectId];
+    const updatedUser = Object.assign({}, byIdState[userId], userProjectsWithNewProject);
+
+    return Object.assign({}, byIdState, {
+        [userId]: updatedUser
+    });
+}
+
+function removeProjectFromUser(byIdState, action) {
+    const userId = action.user;
+    const projectId = action.id;
+
+    let userProjects = [...byIdState[userId].projects];
+    let projectsWithoutRemovedProject = userProjects.filter(project => project !== projectId);
+    const updatedUser = Object.assign({}, byIdState[userId], projectsWithoutRemovedProject);
+
+    return Object.assign({}, byIdState, {
+        [userId]: updatedUser
+    });
+}
+
 function addUserAllIds(allIdsState, action) {
     return [...allIdsState, action.id];
 }
@@ -38,6 +65,8 @@ function reduceById(byIdState, action) {
     switch(action.type) {
         case ADD_USER: return addUserById(byIdState, action);
         case UPDATE_USER: return updateUserById(byIdState, action);
+        case ADD_PROJECT: return addProjectToUser(byIdState, action);
+        case REMOVE_PROJECT: return removeProjectFromUser(byIdState, action);
         default: return byIdState;
     }
 }
